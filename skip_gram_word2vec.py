dbda577476c4cd08315876ae8fb41e69e2bb4a3b
EasonJackson <pyhanson@gmail.com>
2018-10-30 19:45:35 -0400

    Refactor generate_batch function; replace cross_entropy with nce_loss; small fix

--- skip_gram_word2vec.py
+++ skip_gram_word2vec.py
@@ -1,82 +1,128 @@
 import tensorflow as tf
 import re
 import numpy as np
+import collections
 
 
-def get_volcabulary(data):
-    word_list = data.split()
-    volcab = []
-    for word in word_list:
-        word = lower(word)
-        word = re.sub(r'[~a-z]', "", word)
-        if word not in volcab:
-            volcab.append(word)
-    return volcab
+# Constants
+batch_size = 128      # Number of input word sampled per mini batch
+sentence_span = 8     # Number of input word sampled per sentence
+embedding_size = 128  # Dimension of the embedding vector.
+skip_window = 1       # How many words to consider left and right.
+num_skips = 2         # How many times to reuse an input to generate a context.
+
+valid_size = 16       # Random set of words to evaluate similarity on.
+valid_window = 100    # Only pick dev samples in the head of the distribution.
+num_sampled = 60      # NCE sampling rate
 
 
-def word_int_mapping(volcab):
+
+def get_vocabulary(data_raw):
+    '''
+    Argument
+    data_raw - list of [list of strings] cleaned sentences
+
+    Return
+    vocab - list of string, word collections
+    '''
+    vocab = []
+    for sentence in data_raw:
+        word_list = sentence.split()
+        for word in word_list:
+            word = lower(word)
+            word = re.sub(r'[~a-z]', "", word)
+            if word not in vocab:
+                vocab.append(word)
+    return vocab
+
+
+def word_int_mapping(vocab, data_raw):
+    '''
+    Arguments
+    vocab - list of string, word collections
+    data_raw - list of [list of strings] sentences
+
+    Return
+    word_to_int - dict, word to int index mapping
+    int_to_word - dict, int index to word mapping
+    data - list of [list of int], vectorized raw data sequences
+    '''
     word_to_int = {}
     int_to_word = {}
-    for index, word in enumerate(volcab):
+    for index, word in enumerate(vocab):
         word_to_int[word] = index
         int_to_word[index] = word
 
-    return word_to_int, int_to_word
+    data = []
+    for sentence in data_raw:
+        tmp = []
+        for word in sentence.split():
+            tmp.append(word_to_int[word])
+        data.append(tmp)
+
+    return word_to_int, int_to_word, data
 
 
-data_index = 0
-# generate batch data
-def generate_batch(data, batch_size, num_skips, skip_window):
+def generate_batch_sentence(sentence):
     '''
     Arguments:
-    data - list of strings as text data
-    batch_size - int, number of input word in one batch
-    num_skips - int, number of skipping word to pick as prediction
-    skip_window - int, size of skipping window
+    sentence - list of int, vectorized sentence 
 
     Return:
     (batch, context) - tuple of batch input words and context words as labels
     batch - np array of shape (batch_size, )
     context - np array of shape (batch_size, 1)
     '''
-    global data_index
+
     #assert batch_size % num_skips == 0
-    assert num_skips <= 2 * skip_window
+    #assert num_skips <= 2 * skip_window
+
+    batch = []
+    context = []
+
+    # [ skip_window input_word skip_window ]
+    # Randomly select input word index
+    for _ in range(sentence_span // num_skips):
+        # Exclude the first skip_window words and last skip_window words
+        input_index = random.randint(skip_window, len(sentence) - skip_window - 1)
+        for _ in range(num_skips):
+            batch.append(input_index)
+            context_index = random.randint(input_index - skip_window, input_index + skip_window)
+            # Exclude the input word
+            while context_index == input_index:
+                context_index = random.randint(input_index - skip_window, input_index + skip_window)
+            context.append(context_index)
+
+    return batch, context
+
+
+def generate_batch(data):
+    '''
+    Argument
+    data
+    batch - np array of shape (batch_size, )
+    context - np array of shape (batch_size, 1)
+    '''
     batch = np.ndarray(shape=(batch_size), dtype=np.int32)
     context = np.ndarray(shape=(batch_size, 1), dtype=np.int32)
-    span = 2 * skip_window + 1  # [ skip_window input_word skip_window ]
-    buffer = collections.deque(maxlen=span)
-    for _ in range(span):
-        buffer.append(data[data_index])
-        data_index = (data_index + 1) % len(data)
-    for i in range(batch_size // num_skips):
-        target = skip_window  # input word at the center of the buffer
-        targets_to_avoid = [skip_window]
-        for j in range(num_skips):
-            while target in targets_to_avoid:
-                target = random.randint(0, span - 1)
-            targets_to_avoid.append(target)
-            batch[i * num_skips + j] = buffer[skip_window]  # this is the input word
-            context[i * num_skips + j, 0] = buffer[target]  # these are the context words
-        buffer.append(data[data_index])
-        data_index = (data_index + 1) % len(data)
-    # Backtrack a little bit to avoid skipping words in the end of a batch
-    data_index = (data_index + len(data) - span) % len(data)
+    select = np.random.choice(len(data), batch_size // sentence_span, replace=False)
+    tmp_count = 0
+    for index in select:
+        tmp_batch, tmp_context = generate_batch_sentence(data[index])
+        for input_word, context_word in zip(tmp_batch, tmp_context):
+            batch[tmp_count] = input_word
+            context_word[tmp_count] = context_word
+            tmp_count += 1
+
     return batch, context
 
 
 def create_validate():
-    valid_size = 16     # Random set of words to evaluate similarity on.
-    valid_window = 100  # Only pick dev samples in the head of the distribution.
     valid_examples = np.random.choice(valid_window, valid_size, replace=False)
     return valid_examples
 
 
-def create_tensorflow_network():
-    batch_size = 128
-    embedding_size = 128  # Dimension of the embedding vector.
-    skip_window = 1       # How many words to consider left and right.
-    num_skips = 2         # How many times to reuse an input to generate a context.
+def create_tensorflow_network(vocabulary_size):
     valid_examples = create_validate()
 
     # Build input layer
@@ -88,17 +134,33 @@ def create_tensorflow_network():
     embedding_layer = tf.nn.embedding_lookup(embeddings, train_inputs)
 
     # Build output softmax layer
-    weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
-    biases = tf.Variable(tf.zeros([vocabulary_size]))
-    output = tf.matmul(embed, tf.transpose(weights)) + biases
+    ###
+    # weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
+    # biases = tf.Variable(tf.zeros([vocabulary_size]))
+    # output = tf.matmul(embed, tf.transpose(weights)) + biases
+    ###
+    # Optimize time efficiency with noise contrastive estimation
+    nce_weights = tf.Variable(tf.truncated_normal([vocabulary_size, embedding_size], stddev=1.0 / math.sqrt(embedding_size)))
+    nce_biases = tf.Variable(tf.zeros([vocabulary_size]))
 
     # Trasfer labels into one-hot coded vectors
     # Calculate cross entropy through output and training labels
-    train_one_hot = tf.one_hot(train_labels, vocabulary_size)
-    cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=train_one_hot))
+    ###
+    # train_one_hot = tf.one_hot(train_labels, vocabulary_size)
+    # cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output, labels=train_one_hot))
+    ###
+    nce_loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights,
+                                             biases=nce_biases,
+                                             labels=train_labels,
+                                             inputs=embedding_layer,
+                                             num_sampled=num_sampled,
+                                             num_classes=vocabulary_size))
 
-    # Construct the SGD optimizer using a learning rate of 1.0.
-    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(cross_entropy)
+    # Construct the mini batch GD optimizer using a learning rate of 1.0.
+    ###
+    # optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(cross_entropy)
+    ###
+    optimizer = tf.train.GradientDescentOptimizer(1.0).minimize(nce_loss)
 
     # Compute the cosine similarity between minibatch examples and all embeddings.
     norm = tf.sqrt(tf.reduce_sum(tf.square(embeddings), 1, keep_dims=True))
@@ -107,25 +169,30 @@ def create_tensorflow_network():
     valid_embeddings = tf.nn.embedding_lookup(normalized_embeddings, valid_dataset)
     similarity = tf.matmul(valid_embeddings, normalized_embeddings, transpose_b=True)
 
-    return optimizer, cross_entropy, valid_examples, normalized_embeddings, similarity
+    return optimizer, nce_loss, valid_examples, normalized_embeddings, similarity
 
 
-def run():
-    with tf.Session(graph=graph) as session:
+def run(data_raw, num_steps=10000):
+    with tf.Session() as session:
         init_op = tf.global_variables_initializer()
         session.run(init_op)
-        print('Initialized')
 
-        optimizer, cross_entropy, valid_examples, normalized_embeddings, similarity = create_tensorflow_network()
+        optimizer, loss, valid_examples, normalized_embeddings, similarity = create_tensorflow_network()
+        print("Complete neural network structure")
+
+        vocab = get_vocabulary(data_raw)
+        vocabulary_size = len(vocab)
+
+        word_to_int, int_to_word, data = word_int_mapping(vocab, data_raw)
 
         average_loss = 0
         for step in range(num_steps):
-            batch_inputs, batch_context = generate_batch(data, batch_size, num_skips, skip_window)
+            batch_inputs, batch_context = generate_batch(data)
             feed_dict = {train_inputs: batch_inputs, train_context: batch_context}
 
             # We perform one update step by evaluating the optimizer op (including it
             # in the list of returned values for session.run()
-            _, loss_val = session.run([optimizer, cross_entropy], feed_dict=feed_dict)
+            _, loss_val = session.run([optimizer, loss], feed_dict=feed_dict)
             average_loss += loss_val
 
             if step % 2000 == 0:
